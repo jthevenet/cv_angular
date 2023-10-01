8e8ee13a4d62ae1483b7aa09f6dca47814af1f86
jthevenet <jules.thevenet@gmail.com>
2023-10-01 14:32:58 +0200

    add skills intro and timeline components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IntroComponent } from './intro/intro.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    TimelineComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
